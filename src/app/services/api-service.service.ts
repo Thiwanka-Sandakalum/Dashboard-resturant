7331826baadb9c3484e905bc0f78a21d82ac255c
Thiwanka-Sandakalum <thiwanka2002sandakalum@gmail.com>
2023-10-17 17:06:34 +0530

    api

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -6,7 +6,7 @@ import { Observable, catchError, of } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiServiceService {
-  private url = 'https://resturant.azurewebsites.net/api'
+  private url = 'https://restaurant-milanium.azurewebsites.net/api'
 
     constructor(private http: HttpClient) { }
 
